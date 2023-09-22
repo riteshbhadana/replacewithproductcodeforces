dcbfed2ec928f153bb3db15b67a53ac9db6beb95
Ritesh <142724392+riteshbhadana@users.noreply.github.com>
2023-09-22 17:11:06 +0530

    Update 898codeforces.cpp

--- 898codeforces.cpp
+++ 898codeforces.cpp
@@ -252,3 +252,77 @@ int32_t main() {
 	
 	return 0;
 } ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+	#include <bits/stdc++.h>
+using namespace std;
+ 
+#ifndef ONLINE_JUDGE
+#include "algo/debug.h"
+#else
+#define dbg(...) ; 
+#define debug(...) ; 
+#define crndl ; 
+#endif
+ 
+void solve() {
+	
+	string str;
+	cin >> str;
+	
+	int n = str.length();
+	
+	vector<int> a;
+	int zero = 0;
+	int sum = 0;
+	int mn = n;
+	
+	for(int start = 0; start < n; start++) {
+		
+		if(str[start] == 'B') {
+			zero++;
+			continue;
+		}
+		
+		int end = start;
+		while (end < n && str[end] != 'B') end++;
+		
+		int cnt = end - start;
+		a.push_back(cnt);
+		sum += cnt;
+		mn = min(mn, cnt);
+		
+		start = end - 1;
+	}
+	
+	if(zero >= a.size()) {
+		mn = 0;
+	}
+	
+	cout << sum - mn << endl;
+	
+}
+ 
+int32_t main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+	#ifndef ONLINE_JUDGE
+    freopen("io/input.txt", "r", stdin);
+    freopen("io/output.txt", "w", stdout);
+    freopen("io/error.txt", "w", stderr);
+    #endif
+	
+	int t = 1;
+	cin >> t;
+	
+	for(int _t = 1; _t <= t; _t++) {
+		debug(Testcase, _t);
+		solve();
+		crndl;
+	}
+	
+	return 0;
+}
+
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	
